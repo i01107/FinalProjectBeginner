c9a941324eb50c2fcad86095daf4d8b72561fd0a
Fiqi Fitransyah <fiqi.i01107@gmail.com>
2016-12-01 01:32:39 +0700

    Initial commit

---
+++ app/src/main/java/app/com/example/android/finalprojectbeginner/CircleActivity.java
@@ -0,0 +1,23 @@
+package app.com.example.android.finalprojectbeginner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class CircleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_circle);
+
+        Button back = (Button) findViewById(R.id.back_btn);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/app/com/example/android/finalprojectbeginner/MainActivity.java
@@ -0,0 +1,63 @@
+package app.com.example.android.finalprojectbeginner;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RelativeLayout square_ll = (RelativeLayout) findViewById(R.id.square);
+        square_ll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SquareActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        RelativeLayout rectangle_ll = (RelativeLayout) findViewById(R.id.rectangle);
+        rectangle_ll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RectangleActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        RelativeLayout trapezoid_ll = (RelativeLayout) findViewById(R.id.trapezoid);
+        trapezoid_ll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, TrapezoidActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        RelativeLayout circle_ll = (RelativeLayout) findViewById(R.id.circle);
+        circle_ll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CircleActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        RelativeLayout triangle_ll = (RelativeLayout) findViewById(R.id.triangle);
+        triangle_ll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, TriangleActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
