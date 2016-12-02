d98731d716bb34345c7f47da6fa4cc4e9809a1a5
fiqi.i01107@gmail.com <ikhwan1982FFDR>
2016-12-02 18:54:37 +0700

    update + quiz page

--- app/src/main/java/app/com/example/android/finalprojectbeginner/QuizActivity.java
+++ app/src/main/java/app/com/example/android/finalprojectbeginner/QuizActivity.java
@@ -2,6 +2,8 @@ package app.com.example.android.finalprojectbeginner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class QuizActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class QuizActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        Button back = (Button) findViewById(R.id.back_btn);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
